9a4657c53c8974b5d95a88b553a802a0446bff4c
w0qs1 <mnsandeepkumar@gmail.com>
2025-04-15 07:34:57 +0530

    Initial Commit

---
+++ obj/Peripheral/src/ch32v00x_exti.d
@@ -0,0 +1,70 @@
+Peripheral/src/ch32v00x_exti.o: ../Peripheral/src/ch32v00x_exti.c \
+ c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Peripheral/inc/ch32v00x_exti.h \
+ c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Peripheral/inc/ch32v00x.h \
+ c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Core/core_riscv.h \
+ c:/Users/mnsan/mounriver-studio-projects/rp_project_application/User/system_ch32v00x.h \
+ c:/Users/mnsan/mounriver-studio-projects/rp_project_application/User/ch32v00x_conf.h \
+ c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Peripheral/inc/ch32v00x_adc.h \
+ c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Peripheral/inc/ch32v00x_dbgmcu.h \
+ c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Peripheral/inc/ch32v00x_dma.h \
+ c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Peripheral/inc/ch32v00x_flash.h \
+ c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Peripheral/inc/ch32v00x_gpio.h \
+ c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Peripheral/inc/ch32v00x_i2c.h \
+ c:/Users/mnsan/mounriver-studio-projects/rp_project_application/User/ch32v00x_it.h \
+ c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Debug/debug.h \
+ c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Peripheral/inc/ch32v00x_iwdg.h \
+ c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Peripheral/inc/ch32v00x_misc.h \
+ c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Peripheral/inc/ch32v00x_pwr.h \
+ c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Peripheral/inc/ch32v00x_rcc.h \
+ c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Peripheral/inc/ch32v00x_spi.h \
+ c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Peripheral/inc/ch32v00x_tim.h \
+ c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Peripheral/inc/ch32v00x_usart.h \
+ c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Peripheral/inc/ch32v00x_wwdg.h \
+ c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Peripheral/inc/ch32v00x_opa.h \
+ c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Peripheral/inc/ch32v00x.h
+
+c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Peripheral/inc/ch32v00x_exti.h:
+
+c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Peripheral/inc/ch32v00x.h:
+
+c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Core/core_riscv.h:
+
+c:/Users/mnsan/mounriver-studio-projects/rp_project_application/User/system_ch32v00x.h:
+
+c:/Users/mnsan/mounriver-studio-projects/rp_project_application/User/ch32v00x_conf.h:
+
+c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Peripheral/inc/ch32v00x_adc.h:
+
+c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Peripheral/inc/ch32v00x_dbgmcu.h:
+
+c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Peripheral/inc/ch32v00x_dma.h:
+
+c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Peripheral/inc/ch32v00x_flash.h:
+
+c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Peripheral/inc/ch32v00x_gpio.h:
+
+c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Peripheral/inc/ch32v00x_i2c.h:
+
+c:/Users/mnsan/mounriver-studio-projects/rp_project_application/User/ch32v00x_it.h:
+
+c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Debug/debug.h:
+
+c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Peripheral/inc/ch32v00x_iwdg.h:
+
+c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Peripheral/inc/ch32v00x_misc.h:
+
+c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Peripheral/inc/ch32v00x_pwr.h:
+
+c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Peripheral/inc/ch32v00x_rcc.h:
+
+c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Peripheral/inc/ch32v00x_spi.h:
+
+c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Peripheral/inc/ch32v00x_tim.h:
+
+c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Peripheral/inc/ch32v00x_usart.h:
+
+c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Peripheral/inc/ch32v00x_wwdg.h:
+
+c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Peripheral/inc/ch32v00x_opa.h:
+
+c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Peripheral/inc/ch32v00x.h:
---
+++ obj/Startup/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# MRS Version: 2.1.0
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+S_UPPER_SRCS += \
+../Startup/startup_ch32v00x.S 
+
+S_UPPER_DEPS += \
+./Startup/startup_ch32v00x.d 
+
+OBJS += \
+./Startup/startup_ch32v00x.o 
+
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Startup/%.o: ../Startup/%.S
+	@echo 'Building file: $<'
+	@echo 'Invoking: GNU RISC-V Cross C Compiler'
+	riscv-none-embed-gcc -march=rv32ecxw -mabi=ilp32e -msmall-data-limit=0 -msave-restore -fmax-errors=20 -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-common -Wunused -Wuninitialized -g -x assembler-with-cpp -I"c:/Users/mnsan/mounriver-studio-projects/rp_project_application/Startup" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@
